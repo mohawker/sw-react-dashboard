8c83bdfbbe7b7a5dec7db9e23b637c21c8c07595
Jun Jia <junjia2210@gmail.com>
2020-02-26 15:51:12 +0800

    Updates

---
+++ precache-manifest.c1ed19e3adbbf1fbfbcef9650c927fc7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ced94e7349ac423ad21a463604057d8",
+    "url": "/sw-react-uimockup/index.html"
+  },
+  {
+    "revision": "a54a0e5f98dc640de52a",
+    "url": "/sw-react-uimockup/static/css/main.1782a880.chunk.css"
+  },
+  {
+    "revision": "fbc8133b76ea73ea8973",
+    "url": "/sw-react-uimockup/static/js/2.6a44bf0a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sw-react-uimockup/static/js/2.6a44bf0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a54a0e5f98dc640de52a",
+    "url": "/sw-react-uimockup/static/js/main.ca48e8cc.chunk.js"
+  },
+  {
+    "revision": "2839780a88ba8518f0a7",
+    "url": "/sw-react-uimockup/static/js/runtime-main.6079e4a3.js"
+  },
+  {
+    "revision": "feface72008e6857da27f939e1eceffa",
+    "url": "/sw-react-uimockup/static/media/Proxima_Nova.feface72.ttf"
+  },
+  {
+    "revision": "a545f7c0dbe07e41a1d650880d00e3be",
+    "url": "/sw-react-uimockup/static/media/caret-down.a545f7c0.svg"
+  },
+  {
+    "revision": "13edb3ccfceecfb23a77f74418232244",
+    "url": "/sw-react-uimockup/static/media/chemical-loading-gif.13edb3cc.gif"
+  },
+  {
+    "revision": "62c57ded4c7a2c18e2c4b4cf472bc2d7",
+    "url": "/sw-react-uimockup/static/media/icon-campaign.62c57ded.svg"
+  },
+  {
+    "revision": "b825e8cf610c243ed0d9ed7986a5bfb0",
+    "url": "/sw-react-uimockup/static/media/icon-conversations-small.b825e8cf.svg"
+  },
+  {
+    "revision": "175c11b89db829c200e327743c823708",
+    "url": "/sw-react-uimockup/static/media/icon-leads-small.175c11b8.svg"
+  },
+  {
+    "revision": "a8561513a4750e955418a8de274a512a",
+    "url": "/sw-react-uimockup/static/media/icon-leads.a8561513.svg"
+  },
+  {
+    "revision": "f3cfd0cbc1fd596ae1f988e54e1c8703",
+    "url": "/sw-react-uimockup/static/media/icon-mail.f3cfd0cb.svg"
+  },
+  {
+    "revision": "9ab34b7e8e432069868af8461366760b",
+    "url": "/sw-react-uimockup/static/media/icon-reports.9ab34b7e.svg"
+  },
+  {
+    "revision": "7d19281ba4b2ebb22a967176f641792e",
+    "url": "/sw-react-uimockup/static/media/icon-search.7d19281b.svg"
+  },
+  {
+    "revision": "6301f1b2dcfc4c12ded0927226927007",
+    "url": "/sw-react-uimockup/static/media/icon-teams.6301f1b2.svg"
+  },
+  {
+    "revision": "4d5cf6a3c2f988c82c51c891ad4a9b54",
+    "url": "/sw-react-uimockup/static/media/logoAdd.4d5cf6a3.svg"
+  },
+  {
+    "revision": "c8a8636341b97ab2aae8d39a93e17949",
+    "url": "/sw-react-uimockup/static/media/logoTeamsGrey.c8a86363.svg"
+  },
+  {
+    "revision": "18c768c8e958bffd4b822a1c981edeac",
+    "url": "/sw-react-uimockup/static/media/menu-help.18c768c8.svg"
+  },
+  {
+    "revision": "7287f4462fc431dad4fe158c2ba382de",
+    "url": "/sw-react-uimockup/static/media/star-default.7287f446.svg"
+  },
+  {
+    "revision": "6a5c384b55d305a86f373af42f6f80df",
+    "url": "/sw-react-uimockup/static/media/sw-logo-white.6a5c384b.svg"
+  }
+]);
